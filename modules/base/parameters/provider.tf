f7f6da04e54a564679c276efe69602264d15632c
Tarek Mulla <tarek@mulla.au>
2022-11-13 17:24:55 +1100

    Ignore hcl file, and update the required provider

--- modules/base/parameters/provider.tf
+++ modules/base/parameters/provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.34.0"
+      version = ">= 4.34.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.3"
     }
   }
 }
--- modules/website/server/provider.tf
+++ modules/website/server/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.34.0"
+      version = ">= 4.34.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "2.2.0"
+      version = ">= 2.2.0"
     }
   }
 }
