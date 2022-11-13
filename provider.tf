1b6c93e6a3a9b03f4818f89845e632c969fda136
Tarek Mulla <tarek@mulla.au>
2022-11-13 19:27:05 +1100

    Remove all provider files

--- provider.tf
+++ provider.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.34.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.4.3"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-southeast-2"
 }
